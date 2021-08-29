6ff64ee28217ce20e7198181e38416ff053c6af2
zeromsi <www.zeromsi2@gmail.com>
2021-08-29 10:05:43 +0600

    remove index from LogEvent

--- core/v1/log_event.go
+++ core/v1/log_event.go
@@ -3,7 +3,6 @@ package v1
 import "time"
 
 type LogEvent struct {
-	Index     int32     `bson:"index"`
 	ProcessId string    `bson:"process_id"`
 	Log       string    `bson:"log"`
 	Step      string    `bson:"step"`
