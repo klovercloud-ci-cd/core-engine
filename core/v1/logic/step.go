29999a24f4dacd44a695e5ff9b8325302f8e606e
shabrul <shabrul2451@gmail.com>
2021-10-07 13:34:03 +0600

    Implemented test for step of logic layer

--- core/v1/logic/step.go
+++ core/v1/logic/step.go
@@ -11,121 +11,123 @@ type stepService struct {
 	step v1.Step
 }
 
-
-func (service *stepService)SetInput(url,revision string){
-	if service.step.Type==enums.BUILD{
-		service.step.Params[enums.URL]=url
-		service.step.Params[enums.REVISION]=revision
+func (service *stepService) SetInput(url, revision string) {
+	if service.step.Type == enums.BUILD {
+		service.step.Params[enums.URL] = url
+		service.step.Params[enums.REVISION] = revision
 	}
 }
 
-func (service *stepService)SetArgs(k8s service.K8s){
-	service.step.ArgData=map[string]string{}
-	for _,each:=range strings.Split(service.step.Params[enums.ARGS],","){
-			arg:=strings.Split(each,":")
-			if len(arg)==2{
-				service.step.ArgData[arg[0]]=arg[1]
+func (service *stepService) SetArgs(k8s service.K8s) {
+	service.step.ArgData = map[string]string{}
+	if service.step.Params[enums.ARGS] != "" {
+		for _, each := range strings.Split(service.step.Params[enums.ARGS], ",") {
+			arg := strings.Split(each, ":")
+			if len(arg) == 2 {
+				service.step.ArgData[arg[0]] = arg[1]
 			}
-			if len(arg)==1{
-				service.step.ArgData[arg[0]]=""
+			if len(arg) == 1 {
+				service.step.ArgData[arg[0]] = ""
 			}
+		}
 	}
-	for _,each:=range strings.Split(service.step.Params[enums.ARGS_FROM_SECRETS],","){
-	    secret:=strings.Split(each,"/")
-	    var name,namespace string
-		if len(secret)==2{
-			name=secret[0]
-			namespace=secret[1]
+	for _, each := range strings.Split(service.step.Params[enums.ARGS_FROM_SECRETS], ",") {
+		secret := strings.Split(each, "/")
+		var name, namespace string
+		if len(secret) == 2 {
+			name = secret[0]
+			namespace = secret[1]
 		}
-		if len(secret)==1{
-			name=secret[0]
-			namespace="default"
+		if len(secret) == 1 {
+			name = secret[0]
+			namespace = "default"
 		}
-		sec,err:=k8s.GetSecret(name,namespace)
-		if err!=nil{
+		sec, err := k8s.GetSecret(name, namespace)
+		if err != nil {
 			continue
 		}
-		for key,val:=range sec.StringData{
-			service.step.ArgData[key]=string(val)
+		for key, val := range sec.StringData {
+			service.step.ArgData[key] = string(val)
 		}
 	}
-	for _,each:=range strings.Split(service.step.Params[enums.ARGS_FROM_CONFIGMAPS],","){
-		configMap:=strings.Split(each,"/")
+	for _, each := range strings.Split(service.step.Params[enums.ARGS_FROM_CONFIGMAPS], ",") {
+		configMap := strings.Split(each, "/")
 
-		var name,namespace string
-		if len(configMap)==2{
-			name=configMap[0]
-			namespace=configMap[1]
+		var name, namespace string
+		if len(configMap) == 2 {
+			namespace = configMap[0]
+			name = configMap[1]
 		}
-		if len(configMap)==1{
-			name=configMap[0]
-			namespace="default"
+		if len(configMap) == 1 {
+			name = configMap[0]
+			namespace = "default"
 		}
-		cm,err:=k8s.GetConfigMap(name,namespace)
-		if err!=nil{
+		cm, err := k8s.GetConfigMap(name, namespace)
+		if err != nil {
 			continue
 		}
-		for key,val:=range cm.Data{
-			service.step.ArgData[key]=val
+		for key, val := range cm.Data {
+			service.step.ArgData[key] = val
 		}
 
 	}
 }
 
-func  (service *stepService)SetEnvs(k8s service.K8s){
-	service.step.EnvData=map[string]string{}
-
-	for _,each:=range strings.Split(service.step.Params[enums.ENVS],","){
-		env:=strings.Split(each,":")
-		if len(env)==2{
-			service.step.EnvData[env[0]]=env[1]
-		}
-		if len(env)==1{
-			service.step.EnvData[env[0]]=""
-		}
-	}
-	for _,each:=range strings.Split(service.step.Params[enums.ENVS_FROM_SECRETS],","){
-		secret:=strings.Split(each,"/")
-		var name,namespace string
-		if len(secret)==2{
-			name=secret[0]
-			namespace=secret[1]
-		}
-		if len(secret)==1{
-			name=secret[0]
-			namespace="default"
-		}
-		sec,err:=k8s.GetSecret(name,namespace)
-		if err!=nil{
-			continue
-		}
-		for key,val:=range sec.StringData{
-			service.step.EnvData[key]=string(val)
+func (service *stepService) SetEnvs(k8s service.K8s) {
+	service.step.EnvData = map[string]string{}
+	if service.step.Params[enums.ENVS] != "" {
+		for _, each := range strings.Split(service.step.Params[enums.ENVS], ",") {
+			env := strings.Split(each, ":")
+			if len(env) == 2 {
+				service.step.EnvData[env[0]] = env[1]
+			}
+			if len(env) == 1 {
+				service.step.EnvData[env[0]] = ""
+			}
 		}
 	}
-	for _,each:=range strings.Split(service.step.Params[enums.ENVS_FROM_CONFIGMAPS],","){
-		configMap:=strings.Split(each,"/")
 
-		var name,namespace string
-		if len(configMap)==2{
-			name=configMap[0]
-			namespace=configMap[1]
+	for _, each := range strings.Split(service.step.Params[enums.ENVS_FROM_SECRETS], ",") {
+		secret := strings.Split(each, "/")
+		var name, namespace string
+		if len(secret) == 2 {
+			name = secret[0]
+			namespace = secret[1]
 		}
-		if len(configMap)==1{
-			name=configMap[0]
-			namespace="default"
+		if len(secret) == 1 {
+			name = secret[0]
+			namespace = "default"
 		}
-		cm,err:=k8s.GetConfigMap(name,namespace)
-		if err!=nil{
+		sec, err := k8s.GetSecret(name, namespace)
+		if err != nil {
 			continue
 		}
-		for key,val:=range cm.Data{
-			service.step.EnvData[key]=val
+		for key, val := range sec.StringData {
+			service.step.EnvData[key] = string(val)
+		}
+	}
+	for _, each := range strings.Split(service.step.Params[enums.ENVS_FROM_CONFIGMAPS], ",") {
+		configMap := strings.Split(each, "/")
+
+		var name, namespace string
+		if len(configMap) == 2 {
+			namespace = configMap[0]
+			name = configMap[1]
+		}
+		if len(configMap) == 1 {
+			name = configMap[0]
+			namespace = "default"
+		}
+		cm, err := k8s.GetConfigMap(name, namespace)
+		if err != nil {
+			continue
+		}
+		for key, val := range cm.Data {
+			service.step.EnvData[key] = val
 		}
 
 	}
 }
-
 
 func NewStepService(step v1.Step) service.Step {
 	return &stepService{
--- core/v1/logic/step_test.go
+++ core/v1/logic/step_test.go
@@ -37,145 +37,64 @@ func TestStepService_SetInput(t *testing.T) {
 	if !reflect.DeepEqual(testCase.expected, testCase.actual) {
 		assert.ElementsMatch(t, testCase.expected, testCase.actual)
 	}
-
-	service.step.Params[enums.ARGS_FROM_CONFIGMAPS]="klovercloud/secret1"
-	service.SetArgs(NewMockK8sService(nil))
-	fmt.Println(service.step.ArgData)
 }
 
-//
-//import (
-//	v1 "github.com/klovercloud-ci/core/v1"
-//	"github.com/klovercloud-ci/enums"
-//	"github.com/stretchr/testify/assert"
-//	corev1 "k8s.io/api/core/v1"
-//	"reflect"
-//	"testing"
-//)
-//
-//
-//func Test_SetEnvs(t *testing.T) {
-//	type TestCase struct {
-//		configMaps [] corev1.ConfigMap
-//		secrets [] corev1.Secret
-//		expected map[string]string
-//		actual map[string]string
-//	}
-//
-//	testCase:= TestCase{
-//		configMaps:     InitConfigMaps(),
-//		secrets: InitSecrets(),
-//		expected: map[string]string{"key1":"value1","key2":"value2","env1":"value1","env2":"value2"},
-//	}
-//
-//	step := &v1.Step{
-//		Env: v1.Variable{
-//			Secrets: []struct {
-//				Name      string `json:"name" yaml:"name"`
-//				Namespace string `json:"namespace" yaml:"namespace"`
-//			}([]struct {
-//				Name      string `json:"name"`
-//				Namespace string `json:"namespace"`
-//			}{{Name: "secret0", Namespace: "klovercloud"}}),
-//			ConfigMaps: []struct {
-//				Name      string `json:"name" yaml:"name"`
-//				Namespace string `json:"namespace" yaml:"namespace"`
-//			}([]struct {
-//				Name      string `json:"name"`
-//				Namespace string `json:"namespace"`
-//			}{{Name: "configMap0", Namespace: "klovercloud"}}),
-//		},
-//	}
-//	service:=stepService{
-//		step: *step,
-//	}
-//	service.SetEnvs(&mockK8sService{})
-//	testCase.actual=service.step.Env.Data
-//	if !reflect.DeepEqual(testCase.expected, testCase.actual){
-//		assert.ElementsMatch(t, testCase.expected, testCase.actual)
-//	}
-//}
-//
-//func Test_SetArgs(t *testing.T) {
-//	type TestCase struct {
-//		configMaps [] corev1.ConfigMap
-//		secrets [] corev1.Secret
-//		expected map[string]string
-//		actual map[string]string
-//	}
-//
-//	testCase:= TestCase{
-//		configMaps:     InitConfigMaps(),
-//		secrets: InitSecrets(),
-//		expected: map[string]string{"key1":"value1","key2":"value2","env1":"value1","env2":"value2"},
-//		actual:   nil,
-//	}
-//
-//	step := &v1.Step{
-//		Arg: v1.Variable{
-//			Secrets: []struct {
-//				Name      string `json:"name" yaml:"name"`
-//				Namespace string `json:"namespace" yaml:"namespace"`
-//			}([]struct {
-//				Name      string `json:"name"`
-//				Namespace string `json:"namespace"`
-//			}{{Name: "secret0", Namespace: "klovercloud"}}),
-//			ConfigMaps: []struct {
-//				Name      string `json:"name" yaml:"name"`
-//				Namespace string `json:"namespace" yaml:"namespace"`
-//			}([]struct {
-//				Name      string `json:"name"`
-//				Namespace string `json:"namespace"`
-//			}{{Name: "configMap0", Namespace: "klovercloud"}}),
-//		},
-//	}
-//	service:=stepService{
-//		step: *step,
-//	}
-//	service.SetArgs(&mockK8sService{})
-//	testCase.actual=service.step.Arg.Data
-//	if !reflect.DeepEqual(testCase.expected, testCase.actual){
-//		assert.ElementsMatch(t, testCase.expected, testCase.actual)
-//	}
-//}
-//
-//func Test_SetInput(t *testing.T) {
-//	type TestCase struct {
-//		url,revision string
-//		step_type enums.STEP_TYPE
-//		expected v1.Resource
-//		actual v1.Resource
-//	}
-//	step := v1.Step{
-//		Type: enums.BUILD,
-//	}
-//	testCase:=TestCase{
-//		url:       "github.com/abc",
-//		revision:  "1222",
-//		step_type: enums.BUILD,
-//		expected:  v1.Resource{
-//			Url:      "github.com/abc",
-//			Revision: "1222",
-//		},
-//	}
-//
-//	service:=stepService{
-//		step: step,
-//	}
-//	service.SetInput("github.com/abc","1222")
-//	testCase.actual=service.step.Input
-//
-//	if !reflect.DeepEqual(testCase.expected, testCase.actual){
-//		assert.ElementsMatch(t, testCase.expected, testCase.actual)
-//	}
-//	testCase=TestCase{
-//		url:       "github.com/abc",
-//		revision:  "1222",
-//		step_type: enums.DEPLOY,
-//		expected:  v1.Resource{},
-//	}
-//	service.SetInput("github.com/abc","1222")
-//	if !reflect.DeepEqual(testCase.expected, testCase.actual){
-//		assert.ElementsMatch(t, testCase.expected, testCase.actual)
-//	}
-//}
+func TestStepService_SetArgs(t *testing.T) {
+	type TestCase struct {
+		data     v1.Step
+		expected map[string]string
+		actual   map[string]string
+	}
+	testCase := TestCase{
+		expected: map[string]string{"env1": "value1", "env2": "value2"},
+	}
+	step := &v1.Step{
+		Name:        "build",
+		Type:        "BUILD",
+		Trigger:     "AUTO",
+		Params:      map[enums.PARAMS]string{"args_from_configmaps": "klovercloud/configMap1"},
+		Next:        nil,
+		ArgData:     nil,
+		EnvData:     nil,
+		Descriptors: nil,
+	}
+	service := stepService{
+		step: *step,
+	}
+	service.SetArgs(NewMockK8sService(nil))
+	testCase.actual = service.step.ArgData
+	if !reflect.DeepEqual(testCase.expected, testCase.actual) {
+		fmt.Println(testCase.expected, testCase.actual)
+		assert.ElementsMatch(t, testCase.expected, testCase.actual)
+	}
+}
+
+func TestStepService_SetEnvs(t *testing.T) {
+	type TestCase struct {
+		data     v1.Step
+		expected map[string]string
+		actual   map[string]string
+	}
+	testCase := TestCase{
+		expected: map[string]string{"env1": "value1", "env2": "value2"},
+	}
+	step := &v1.Step{
+		Name:        "build",
+		Type:        "BUILD",
+		Trigger:     "AUTO",
+		Params:      map[enums.PARAMS]string{"envs_from_configmaps": "klovercloud/configMap1"},
+		Next:        nil,
+		ArgData:     nil,
+		EnvData:     nil,
+		Descriptors: nil,
+	}
+	service := stepService{
+		step: *step,
+	}
+	service.SetEnvs(NewMockK8sService(nil))
+	testCase.actual = service.step.EnvData
+	if !reflect.DeepEqual(testCase.expected, testCase.actual) {
+		fmt.Println(testCase.expected, testCase.actual)
+		assert.ElementsMatch(t, testCase.expected, testCase.actual)
+	}
+}
